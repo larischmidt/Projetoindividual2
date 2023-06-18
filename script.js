87ad69d48e3d9f47168ef684c345c7f1919d0a4e
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-06-18 15:12:43 -0300

    alteração no html e css e javaScript

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var entrada = document.getElementById("entrada");
 var saida = document.getElementById("saida");
 var valorDeEntrada = document.getElementById("valorDeEntrada");
 var botao= document.getElementById("botao")
+var resultado= document.getElementById("resultado")
 
 // ------------------------------------------------------------Criando as opções para selecionar --//
 
@@ -40,21 +41,47 @@ categoria.addEventListener("change", function () {
   }
 });
 
-// ------------------------------------------------------------
-
+// ---------------------------------------- Ligando as informações para o botão e assim aparecer o resultado--//
+// .toFixed()
+botao.addEventListener('click', (event) => event.preventDefault());
 botao.addEventListener("click",function(){
+ var valor= +valorDeEntrada.value
   if (entrada.value == "metros" && saida.value === "centimetros") {
-    return (valorDeEntrada.value * 100);
+   resultado.innerText= metrosCentimetros (valor)
   } else if (entrada.value == "centimetros" && saida.value === "metros") {
-    return (valorDeEntrada.value / 100);
+   resultado.innerText= centimentrosMetros (valor)
   } else if (entrada.value == "centimetros" && saida.value === "polegadas") {
-    return (valorDeEntrada.value / 2.54);
+   resultado.innerText= centimentrosPolegadas (valor)
   } else if (entrada.value == "polegadas" && saida.value === "centimetros") {
-    return (valorDeEntrada.value * 2.54);
+   resultado.innerText= polegadasCentimetros (valor)
   } else if (entrada.value == "polegadas" && saida.value === "metros") {
-    return (valorDeEntrada.value / 39.37);
+   resultado.innerText= polegadasMetros (valor)
   } else if (entrada.value == "metros" && saida.value === "polegadas") {
-    return (valorDeEntrada.value * 39.37);
+   resultado.innerText= metrosPolegadas (valor)
+  }  if (entrada.value == "celsius" && saida.value === "fahrenheit") {
+    resultado.innerText= celsiusFahrenheit (valor)
+  } else if (entrada.value == "fahrenheit" && saida.value === "celsius") {
+    resultado.innerText= fahrenheitCelsius (valor)
+  } else if (entrada.value == "celsius" && saida.value === "kelvin") {
+    resultado.innerText= celsiusKelvin (valor)
+  } else if (entrada.value == "kelvin" && saida.value === "celsius") {
+    resultado.innerText= kelvinCelsius (valor)
+  } else if (entrada.value == "fahrenheit" && saida.value === "kelvin") {
+    resultado.innerText= fahrenheitKelvin (valor)
+  } else if (entrada.value == "kelvin" && saida.value === "fahrenheit") {
+    resultado.innerText= kelvinfahrenheit (valor)
+  } if (entrada.value == "quilograma" && saida.value === "gramas") {
+    resultado.innerText= quilogramaGramas (valor)
+  } else if (entrada.value == "gramas" && saida.value === "quilograma") {
+    resultado.innerText= gramasQuilograma (valor)
+  } else if (entrada.value == "gramas" && saida.value === "libras") {
+    resultado.innerText= gramasLibras (valor)
+  } else if (entrada.value == "libras" && saida.value === "gramas") {
+    resultado.innerText= librasGramas (valor)
+  } else if (entrada.value == "libras" && saida.value === "quilograma") {
+    resultado.innerText= librasQuilograma (valor)
+  } else if (entrada.value == "quilograma" && saida.value === "libras") {
+    resultado.innerText= quilogramaLibras (valor)
   }
 
 })
@@ -71,6 +98,7 @@ function centimentrosMetros(centimentros){
 }
 function centimentrosPolegadas(centimetros){
 var polegadas= (centimetros / 2.54);
+return polegadas;
 }
 function polegadasCentimetros(polegadas){
   var centimetros=(polegadas * 2.54);
@@ -88,11 +116,11 @@ function metrosPolegadas(metros){
 // ---------------------------------------------------------unidade de medidade temperatura---//
 
 function celsiusFahrenheit(celsius){
-  var fahrenheit=(celsius * 9 / 5) + 32;
+  var fahrenheit=(celsius * 9/5) + 32;
   return fahrenheit;
 }
 function fahrenheitCelsius(fahrenheit){
-  var celsius=(fahrenheit - 32) * 5 / 9;
+  var celsius=(fahrenheit - 32) * 5/9;
   return celsius;
 }
 function celsiusKelvin(celsius){
@@ -104,26 +132,26 @@ function kelvinCelsius(kelvin){
   return celsius;
 }
 function fahrenheitKelvin(fahrenheit){
-  var kelvin=(fahrenheit - 32)* 5 / 9 + 273.15;
+  var kelvin=(fahrenheit - 32)* 5/9 + 273.15;
   return kelvin;
 }
 function kelvinfahrenheit(kelvin){
-  var fahrenheit=( kelvin - 273.15)* 9 / 5 + 32;
+  var fahrenheit=( kelvin - 273.15)* 9/5 + 32;
   return fahrenheit;
 }
 
 // --------------------------------------------------------- unidade de medida peso ----//
 
 function quilogramaGramas(quilograma){
-  var gramas=(quilograma * 1000);
+  var gramas=(quilograma *1000);
   return gramas;
 }
 function gramasQuilograma(gramas){
-  var quilograma=(gramas / 1000);
+  var quilograma=(gramas /1000);
   return quilograma;
 }
 function gramasLibras(gramas){
-  var libras=(gramas / 453.6);
+  var libras=(gramas /453.6);
   return libras;
 }
 function librasGramas(libras){
@@ -131,7 +159,7 @@ function librasGramas(libras){
   return gramas;
 }
 function librasQuilograma(libras){
-  var quilograma=(libras / 2.205);
+  var quilograma=(libras /2.205);
   return quilograma;
 }
 function quilogramaLibras(quilograma){
@@ -139,51 +167,6 @@ function quilogramaLibras(quilograma){
   return libras;
 }
 
-// function converteComprimento() {
-//   if (entrada.value == "metros" && saida.value === "centimetros") {
-//     return (valorDeEntrada.value * 100);
-//   } else if (entrada.value == "centimetros" && saida.value === "metros") {
-//     return (valorDeEntrada.value / 100);
-//   } else if (entrada.value == "centimetros" && saida.value === "polegadas") {
-//     return (valorDeEntrada.value / 2.54);
-//   } else if (entrada.value == "polegadas" && saida.value === "centimetros") {
-//     return (valorDeEntrada.value * 2.54);
-//   } else if (entrada.value == "polegadas" && saida.value === "metros") {
-//     return (valorDeEntrada.value / 39.37);
-//   } else if (entrada.value == "metros" && saida.value === "polegadas") {
-//     return (valorDeEntrada.value * 39.37);
-//   }
-// }
 
-// function converteTemperatura() {
-//   if (entrada.value == "celsius" && saida.value === "fahrenheit") {
-//     return (valorDeEntrada.value * 9) / 5 + 32;
-//   } else if (entrada.value == "fahrenheit" && saida.value === "celsius") {
-//     return (valorDeEntrada.value - 32) * 5 / 9;
-//   } else if (entrada.value == "celsius" && saida.value === "kelvin") {
-//     return (valorDeEntrada.value + 273.15);
-//   } else if (entrada.value == "kelvin" && saida.value === "celsius") {
-//     return (valorDeEntrada.value - 273.15);
-//   } else if (entrada.value == "fahrenheit" && saida.value === "kelvin") {
-//     return (valorDeEntrada.value - 32) * 5 / 9 + 273.15;
-//   } else if (entrada.value == "kelvin" && saida.value === "fahrenheit") {
-//     return (valorDeEntrada.value - 273.15) * 9 / 5 + 32;
-//   }
-// }
 
-// function convertePeso() {
-  // if (entrada.value == "quilograma" && saida.value === "gramas") {
-  //   return (valorDeEntrada.value * 1000);
-  // } else if (entrada.value == "gramas" && saida.value === "quilograma") {
-  //   return (valorDeEntrada.value / 1000);
-  // } else if (entrada.value == "gramas" && saida.value === "libras") {
-  //   return (valorDeEntrada.value / 453.6);
-  // } else if (entrada.value == "libras" && saida.value === "gramas") {
-  //   return (valorDeEntrada.value * 453.6);
-  // } else if (entrada.value == "libras" && saida.value === "quilograma") {
-  //   return (valorDeEntrada.value / 2.205);
-//   } else if (entrada.value == "quilograma" && saida.value === "libras") {
-//     return (valorDeEntrada.value *2.205);
-//   }
-// }
 
